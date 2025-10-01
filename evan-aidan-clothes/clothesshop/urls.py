a99a60a52a95cb122c330a6e44835a60115c158b
emurrin14 <easye879@gmail.com>
2025-10-01 19:15:48 +0000

    dhsa

--- evan-aidan-clothes/clothesshop/urls.py
+++ evan-aidan-clothes/clothesshop/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("listing/<int:listing_id>/", views.listing, name="listing"),
     path("cart/", views.view_cart, name="view_cart"),
+    path("cart/remove/<int:item_id>/", views.remove_from_cart, name="remove_from_cart"),
     path("cart/add/<int:listing_id>/", views.add_to_cart, name="add_to_cart"),
+    path("cart/subtract/<int:item_id>/", views.subtract_from_cart, name="subtract_from_cart"),
     path("profile/", views.profile, name="profile"),
 ]
--- evan-aidan-clothes/clothesshop/views.py
+++ evan-aidan-clothes/clothesshop/views.py
@@ -50,7 +50,7 @@ def add_to_cart(request, listing_id):
         cart_item.save()
 
     # Redirect back to the listing detail page
-    return redirect('listing', listing_id=listing_id)
+    return redirect('view_cart')
 
 def view_cart(request):
     """View to display the cart contents."""
@@ -59,6 +59,32 @@ def view_cart(request):
         'cart': cart
     }
     return render(request, 'clothesshop/cart_detail.html', context)
+
+def remove_from_cart(request, item_id):
+    """View to remove an item from the cart."""
+    cart_item = get_object_or_404(CartItem, pk=item_id)
+    current_cart = _get_cart(request)
+
+    # Ensure the item being removed belongs to the current user's cart
+    if cart_item.cart == current_cart:
+        cart_item.delete()
+
+    # Redirect back to the cart page
+    return redirect('view_cart')
+
+def subtract_from_cart(request, item_id):
+    """View to subtract an item from the cart."""
+    cart_item = get_object_or_404(CartItem, pk=item_id)
+    current_cart = _get_cart(request)
+
+    # Ensure the item belongs to the current user's cart
+    if cart_item.cart == current_cart:
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+
+    return redirect('view_cart')
+
 
 def profile(request):
     return render(request, "clothesshop/Profiles.html")
